26d7e569ab80227fe5d608fd49b6cd0be77fcc1b
murat <muratnysambaev93@gmail.com>
2024-04-03 08:32:39 +0500

    bd connect

--- services/contact/cmd/app/main.go
+++ services/contact/cmd/app/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	postgres "murat/clean_architecture/pkg/store/postres"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	store, err := postgres.New(postgres.Settings{
+		Host:     "localhost",
+		Port:     5432,
+		Database: "test",
+		User:     "user",
+		Password: "password",
+		SSLMode:  false,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer store.Pool.Close()
+
+	store.Pool.Stat()
+
+	fmt.Println("Hello World")
 }
